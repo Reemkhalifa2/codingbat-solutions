63786949b292df21642f509d12ab4d8df152af3a
reem khalifa <aladawi12reem6@gmail.com>
2025-07-27 13:58:57 +0400

    Add solution for warmup1-close10

---
+++ warmup1/close10.java
@@ -0,0 +1,9 @@
+public int close10(int a, int b) {
+  if (Math.abs(a-10) > Math.abs(b-10)){
+            return b;
+        }else if (Math.abs(a-10) < Math.abs(b-10)){
+            return a;
+        }else{
+            return 0;
+        }
+}
