6300d01e788f5fe478bfdfcf2b876f65fa851a04
Reemkhalifa2 <aladawi12reem6@gmail.com>
2025-07-29 11:23:13 +0400

    Update EndsLy.java

--- string1/EndsLy.java
+++ string1/EndsLy.java
@@ -1,1 +1,8 @@
-ECHO is on.
+public boolean endsLy(String str) {
+  if(str.length()>2){
+    return str.substring(str.length() - 2,str.length()).equals("ly");
+  }
+  
+  return str.equals("ly");
+  
+}
