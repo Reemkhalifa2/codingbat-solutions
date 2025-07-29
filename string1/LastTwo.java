6d412b388ba29eae2412199edb24d2c788e43bcb
Reemkhalifa2 <aladawi12reem6@gmail.com>
2025-07-29 12:07:17 +0400

    Update LastTwo.java

--- string1/LastTwo.java
+++ string1/LastTwo.java
@@ -1,1 +1,10 @@
-ECHO is on.
+public String lastTwo(String str) {
+  if(str.length()>2){
+    return str.substring(0,str.length()-2)+str.substring(str.length()-1)+str.substring(str.length()-2,str.length()-1);
+  }else{
+    if(str.length()==0){
+      return "";
+    }  return str.substring(1)+str.substring(0,1);
+
+  }
+}
