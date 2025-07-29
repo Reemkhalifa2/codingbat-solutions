dcf684ad072ee5e9767ad8590d97fba6791288c4
reem khalifa <aladawi12reem6@gmail.com>
2025-07-29 12:57:19 +0400

    Add solution for logic1-teenSum

---
+++ string1/AtFirst.java
@@ -0,0 +1,9 @@
+public String atFirst(String str) {
+  if(str.length()>=2){
+    return str.substring(0,2);
+  }
+  if (str.length() == 0){
+    return "@@";
+  }
+  return str + "@";
+}
--- string1/FirstTwo.java
+++ string1/FirstTwo.java
@@ -1,1 +1,10 @@
+<<<<<<< HEAD
 ECHO is on.
+=======
+public String firstTwo(String str) {
+  if(str.length()>2){
+    return str.substring(0,2);
+  }
+  return str;
+}
+>>>>>>> fbe972ef1c4d09f2db4dbdd2b7698deafc960733
