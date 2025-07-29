0257d208f8b2e336f82b9eefeb0ca90aee4e2e56
Reemkhalifa2 <aladawi12reem6@gmail.com>
2025-07-29 12:12:21 +0400

    Update StartWord.java

--- string1/StartWord.java
+++ string1/StartWord.java
@@ -1,1 +1,9 @@
-ECHO is on.
+public String startWord(String str, String word) {
+  if (str.length() < word.length()) {
+    return "";
+  }
+  if (str.substring(1, word.length()).equals(word.substring(1))) {
+    return str.substring(0, word.length());
+  }
+  return "";
+}
