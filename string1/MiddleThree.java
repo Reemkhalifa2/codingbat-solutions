e15db2d2d31bde8808367df9eb628520029d01d0
Reemkhalifa2 <aladawi12reem6@gmail.com>
2025-07-29 12:08:42 +0400

    Update MiddleThree.java

--- string1/MiddleThree.java
+++ string1/MiddleThree.java
@@ -1,1 +1,9 @@
-ECHO is on.
+public String middleThree(String str) {
+  int middle = (str.length()-3)/2;
+  
+  if(str.length()>3){
+    return str.substring(middle,middle+3);
+  }
+  
+  return str;
+}
