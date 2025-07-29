d174fadf6f254be0d6d2f60bf9e13b8b269b745e
Reemkhalifa2 <aladawi12reem6@gmail.com>
2025-07-29 12:09:40 +0400

    Update MinCat.java

--- string1/MinCat.java
+++ string1/MinCat.java
@@ -1,1 +1,11 @@
-ECHO is on.
+public String minCat(String a, String b) {
+  if (a.length() > b.length()) {
+    int diff = a.length() - b.length();
+    return a.substring(diff) + b;
+  } else if (b.length() > a.length()) {
+    int diff = b.length() - a.length();
+    return a + b.substring(diff);
+  } else {
+    return a + b;
+  }
+}
