9aa712e35fa0cdf1a157cee1b344a40e5fbbef1c
reem khalifa <aladawi12reem6@gmail.com>
2025-07-29 12:53:45 +0400

    Add solution for logic1-more20

--- string1/NonStart.java
+++ string1/NonStart.java
@@ -1,1 +1,9 @@
+<<<<<<< HEAD
 ECHO is on.
+=======
+public String nonStart(String a, String b) {
+  return a.substring(1,a.length())+b.substring(1,b.length());
+  
+  
+}
+>>>>>>> fbe972ef1c4d09f2db4dbdd2b7698deafc960733
