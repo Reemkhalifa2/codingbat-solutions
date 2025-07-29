c22a257b64e688995937721a2ef0e5718d0211b8
Reemkhalifa2 <aladawi12reem6@gmail.com>
2025-07-29 11:26:23 +0400

    Update HasBad.java

--- string1/HasBad.java
+++ string1/HasBad.java
@@ -1,1 +1,11 @@
-ECHO is on.
+public boolean hasBad(String str) {
+  if (str.length() >= 3) {
+    if (str.substring(0, 3).equals("bad")) {
+      return true;
+    }
+    if (str.length() >= 4 && str.substring(1, 4).equals("bad")) {
+      return true;
+    }
+  }
+  return false;
+}
